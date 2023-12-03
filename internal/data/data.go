814ddb40c0405a6d7e1c32b46b92b04b508adaed
1Suro1 <timu.zaxarov@gmail.com>
2023-12-03 12:34:07 +0300

    add db migrate

--- internal/data/data.go
+++ internal/data/data.go
@@ -86,5 +86,6 @@ func NewDB(c *conf.Data) *gorm.DB {
 		log.Errorf("failed opening connection to postgres: %v", err)
 		panic("failed to connect database")
 	}
+	db.AutoMigrate(&Accident{})
 	return db
 }
