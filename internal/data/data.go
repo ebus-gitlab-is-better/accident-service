3b30b3025b5c5d1ea525e22de4a84df0b2b0ba88
1Suro1 <timu.zaxarov@gmail.com>
2023-12-05 01:52:44 +0300

    fix keycloak

--- internal/data/data.go
+++ internal/data/data.go
@@ -4,6 +4,7 @@ import (
 	"accident-service/internal/biz"
 	"accident-service/internal/conf"
 	"context"
+	"crypto/tls"
 	"fmt"
 	slog "log"
 	"os"
@@ -73,6 +74,8 @@ func (d *Data) ExecTx(ctx context.Context, fn func(ctx context.Context) error) e
 
 func NewKeycloak(c *conf.Data) *gocloak.GoCloak {
 	client := gocloak.NewClient(c.Keycloak.Hostname)
+	restyClient := client.RestyClient()
+	restyClient.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
 	return client
 }
 
